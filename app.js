35fafcb196699401ebbb2d714ec15ffad9e46e86
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-11-08 22:52:48 +0600

    finish

--- app.js
+++ app.js
@@ -1,157 +1,102 @@
 // nav-menu
 const navMenu = document.getElementById("nav-menu"),
-    navToggle = document.getElementById("nav-toggle"),
-    navClose = document.getElementById("nav-close");
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 if (navToggle) {
-    navToggle.addEventListener("click", () => {
-        navMenu.classList.add("show-menu");
-    });
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 // nav Close
 if (navClose) {
-    navClose.addEventListener("click", () => {
-        navMenu.classList.remove("show-menu");
-    });
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 const navLink = document.querySelectorAll(".nav__link");
 function linkAction() {
-    const navMenu = document.getElementById("nav-menu");
-    navMenu.classList.remove("show-menu");
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 //skills open close
 const skillsContent = document.getElementsByClassName("skills__content"),
-    skillsHeader = document.querySelectorAll(".skills__header");
+  skillsHeader = document.querySelectorAll(".skills__header");
 
 function toggleSkills() {
-    let itemClass = this.parentNode.className;
-    for (i = 0; i < skillsContent.length; i++) {
-        skillsContent[i].className = "skills__content skills__close";
-    }
-    if (itemClass === "skills__content skills__close") {
-        this.parentNode.className = "skills__content skills__open";
-    }
+  let itemClass = this.parentNode.className;
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
 }
 
 skillsHeader.forEach((el) => {
-    el.addEventListener("click", toggleSkills);
+  el.addEventListener("click", toggleSkills);
 });
 
 //eduction slide
 const tabs = document.querySelectorAll("[data-target]"),
-    tabContents = document.querySelectorAll("[data-content]");
+  tabContents = document.querySelectorAll("[data-content]");
 
 tabs.forEach((tab) => {
-    tab.addEventListener("click", () => {
-        const target = document.querySelector(tab.dataset.target);
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
 
-        tabContents.forEach((tabContent) => {
-            tabContent.classList.remove("qualification__active");
-        });
-        target.classList.add("qualification__active");
-
-        tabs.forEach((tab) => {
-            tab.classList.remove("qualification__active");
-        });
-        tab.classList.add("qualification__active");
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
     });
-});
+    target.classList.add("qualification__active");
 
-//srvices
-const modalViews = document.querySelectorAll(".services__modal"),
-    modalBtns = document.querySelectorAll(".services__button"),
-    modalCloses = document.querySelectorAll(".services__modal-close");
-
-let modal = function (modalClick) {
-    modalViews[modalClick].classList.add("active-modal");
-};
-
-modalBtns.forEach((modalBtn, i) => {
-    modalBtn.addEventListener("click", () => {
-        modal(i);
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
     });
-});
-
-modalCloses.forEach((modalClose) => {
-    modalClose.addEventListener("click", () => {
-        modalViews.forEach((modalView) => {
-            modalView.classList.remove("active-modal");
-        });
-    });
-});
-
-//project slide
-let swiperProtfolio = new Swiper(".protfolio__container", {
-    cssMode: true,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
-
-//testimonial
-let swiperTestimonial = new Swiper(".testimonial__container", {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 48,
-
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-    },
-    breakpoints: {
-        568: {
-            slidesPerView: 2,
-        },
-    },
+    tab.classList.add("qualification__active");
+  });
 });
 
 //footer
 const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
-    const scrollY = window.pageYOffset;
+  const scrollY = window.pageYOffset;
 
-    sections.forEach((current) => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute("id");
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document
-                .querySelector(".nav__menu a[href*=" + sectionId + "]")
-                .classList.add("active-link");
-        } else {
-            document
-                .querySelector(".nav__menu a[href*=" + sectionId + "]")
-                .classList.remove("active-link");
-        }
-    });
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
 }
 window.addEventListener("scroll", scrollActive);
 
 //ching background hider
 function scrollHeader() {
-    const nav = document.getElementById("header");
-    if (this.scrollY >= 80) nav.classList.add("scroll-header");
-    else nav.classList.remove("scroll-header");
+  const nav = document.getElementById("header");
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
 
 //scroll up
 function scrollUp() {
-    const scrollUp = document.getElementById("scroll-up");
-    if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-    else scrollUp.classList.remove("show-scroll");
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
 }
 window.addEventListener("scroll", scrollUp);
 
@@ -164,57 +109,97 @@ const selectedTheme = localStorage.getItem("selected-theme");
 const selectedIcon = localStorage.getItem("selected-icon");
 
 const getCurrentTheme = () =>
-    document.body.classList.contains(darkTheme) ? "dark" : "light";
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
-    themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 
 if (selectedTheme) {
-    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-        darkTheme
-    );
-    themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
-        iconTheme
-    );
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
 themeButton.addEventListener("click", () => {
-    document.body.classList.toggle(darkTheme);
-    themeButton.classList.toggle(iconTheme);
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
 
-    localStorage.setItem("selected-theme", getCurrentTheme());
-    localStorage.setItem("selected-icon", getCurrentIcon());
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
 //contact me
 document.querySelector("form").addEventListener("submit", async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
 
-    try {
-        const response = await fetch(
-            "https://flower-seal.vercel.app/flowers/contact/",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ name, email, message }),
-            }
-        );
+  try {
+    const response = await fetch(
+      "https://flower-seal-backend.vercel.app/flowers/contact/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, message }),
+      }
+    );
 
-        if (response.ok) {
-            alert("Your message has been sent successfully.");
-            location.reload();
-            document.querySelector("form").reset();
-        } else {
-            const errorData = await response.json();
-            alert("Failed to send your message: " + JSON.stringify(errorData));
-        }
-    } catch (error) {
-        console.error("Error:", error);
-        alert("An error occurred while sending your message.");
+    if (response.ok) {
+      alert("Your message has been sent successfully.");
+      location.reload();
+      document.querySelector("form").reset();
+    } else {
+      const errorData = await response.json();
+      alert("Failed to send your message: " + JSON.stringify(errorData));
     }
+  } catch (error) {
+    console.error("Error:", error);
+    alert("An error occurred while sending your message.");
+  }
 });
+
+// Modal Open and Close Functionality
+
+document.addEventListener("DOMContentLoaded", function () {
+  const viewMoreButtons = document.querySelectorAll(".services__button");
+
+  viewMoreButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const modal =
+        this.closest(".portfolio-card").querySelector(".services__modal");
+      modal.style.opacity = "1";
+      modal.style.visibility = "visible";
+    });
+  });
+
+  const modalCloseButtons = document.querySelectorAll(".services__modal-close");
+
+  modalCloseButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const modal = this.closest(".services__modal");
+      modal.style.opacity = "0";
+      modal.style.visibility = "hidden";
+    });
+  });
+});
+
+
+// View More
+document.querySelectorAll('.services__button').forEach(button => {
+    button.addEventListener('click', () => {
+      button.nextElementSibling.style.display = 'flex';
+    });
+  });
+  
+  document.querySelectorAll('.services__modal-close').forEach(closeBtn => {
+    closeBtn.addEventListener('click', (e) => {
+      e.target.closest('.services__modal').style.display = 'none';
+    });
+  });
+  
